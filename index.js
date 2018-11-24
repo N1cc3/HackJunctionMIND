b469117ebc4227dda87fcb206dd2067bf5ff1387
Igor Soroka <stupbird@gmail.com>
2018-11-24 21:02:29 +0200

    Trying to analyze text

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express()
 const serveStatic = require('serve-static')
 const port = process.env.PORT || 3000
 const http = require("http")
-
+const BOT_TOKEN = '40d5fa3351bbc08183f3e9b3f92fba80'
 const superagent = require('superagent');
 const bodyParser = require("body-parser");
+const request = new recastai.request(BOT_TOKEN, 'en')
+let recastai = require('recastai').default
+
 
 // Imports the Google Cloud client library
 const {Translate} = require('@google-cloud/translate');
@@ -28,17 +31,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post("/test", (req,res) => {
-	const conversationId = req.body.conversation.id;
+	// const conversationId = req.body.conversation.id;
+	const originalMessage = req.body.nlp.source;
+	request.analyseText('Where is the event?')
+	.then(function(res) {
+		console.log(res.raw)
+	})
 
-	superagent
-		.post('https://api.recast.ai/connect/v1/conversations/'+conversationId+'/messages')
-		.send({messages: [{ type: 'text', content: 'Hello World' }]})
-		.set('Authorization', 'Token 40d5fa3351bbc08183f3e9b3f92fba80')
-		.end(function(err, res) {
-			console.log(res);
-		});
 
-		res.json({});
+	// superagent
+	// 	.post('https://api.recast.ai/connect/v1/conversations/'+conversationId+'/messages')
+	// 	.send({messages: [{ type: 'text', content: 'Hello World' }]})
+	// 	.set('Authorization', 'Token ' + BOT_TOKEN)
+	// 	.end(function(err, res) {
+	// 		console.log(res);
+	// 	});
+
+	res.json({});
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
