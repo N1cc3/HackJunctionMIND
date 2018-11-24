cb13636e19a7ce8f98a0fe2acb644078053c5a10
N1cc3 <niclaslindgren91@hotmail.com>
2018-11-24 14:50:23 +0200

    Added test get request.

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/disruption", (req,res) => {
 
 })
 
+app.get("/test", (req,res) => {
+	res.send('test');
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // Keep Heroku server alive
