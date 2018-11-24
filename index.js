112e14983a671a8a2f073e3f04d1633ee8bde1e9
N1cc3 <niclaslindgren91@hotmail.com>
2018-11-24 10:23:16 +0200

    Corrected heroku keepalive hack.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const app = express();
+const app = express()
 const serveStatic = require('serve-static')
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 3000
+const http = require("http")
 
 app.use(serveStatic('public'))
 
 app.get("/disruption", (req,res) => {
-    
 
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// Keep Heroku server alive
+setInterval(function() {
+	http.get(process.env.HEROKUAPP)
+}, 300000) // every 5 minutes
