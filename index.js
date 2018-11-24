18e48fd624823a56e0c111a9c2d504006e4f5990
N1cc3 <niclaslindgren91@hotmail.com>
2018-11-24 17:13:56 +0200

    Translation, first try.

--- index.js
+++ index.js
@@ -22,3 +22,41 @@ app.get("/test", (req,res) => {
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+// Imports the Google Cloud client library
+const {Translate} = require('@google-cloud/translate');
+
+// Your Google Cloud Platform project ID
+const projectId = 'alien-paratext-223508';
+
+// Instantiates a client
+const translate = new Translate({
+  projectId: projectId,
+});
+
+
+app.get("/translate/:text", (req, res) => {
+
+	// The text to translate
+	const text = req.params.text;
+	// The target language
+	const target = 'en';
+
+	let result = '';
+	// Translates some text into Russian
+	translate
+		.translate(text, target)
+		.then(results => {
+			const translation = results[0];
+
+			console.log(`Text: ${text}`);
+			console.log(`Translation: ${translation}`);
+
+			result = translation;
+		})
+		.catch(err => {
+			console.error('ERROR:', err);
+		});
+	res.send(result);
+})
