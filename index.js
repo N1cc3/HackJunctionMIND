db0c2aec89e7561a33493dc359d261e96aa5c6d1
N1cc3 <niclaslindgren91@hotmail.com>
2018-11-25 02:32:31 +0200

    Removed unused endpoints.

--- index.js
+++ index.js
@@ -33,19 +33,6 @@ app.get("/disruption", (req,res) => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/test", (req,res) => {
-	const conversationId = req.body.conversation.id;
-	superagent
-		.post('https://api.recast.ai/connect/v1/conversations/'+conversationId+'/messages')
-		.send({messages: [{ type: 'text', content: 'Hello World' }]})
-		.set('Authorization', 'Token ' + BOT_TOKEN)
-		.end(function(err, res) {
-			console.log(res);
-		});
-
-	res.json({});
-})
-
 app.post("/test2", (req, res) => {
 	const originalMessage = req.body.nlp.source;
 	const conversationId = req.body.conversation.id;
@@ -92,50 +79,6 @@ function sendMessageToChat(conversationId, message, lang) {
 			console.error('ERROR:', err);
 		});
 }
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-app.get("/translate/:text", (req, res) => {
-
-	// The text to translate
-	const text = req.params.text;
-	// The target language
-	const target = 'en';
-
-	let sourceLang;
-
-	// Translates some text into Russian
-	translate
-		.translate(text, target)
-		.then(results => {
-			sourceLang = results[1].data.translations[0].detectedSourceLanguage;
-			const translation = results[0];
-
-			console.log(`Text: ${text}`);
-			console.log(`Translation: ${translation}`);
-
-			let result;
-			if (translation === "Hello") {
-				result = "How can I help you?";
-			} else {
-				result = translation;
-			}
-
-			translate
-				.translate(result, sourceLang)
-				.then(results => {
-					const translationBack = results[0];
-					console.log(`Translation back: ${translationBack}`);
-					res.send(translationBack);
-				})
-				.catch(err => {
-					console.error('ERROR:', err);
-				});
-		})
-		.catch(err => {
-			console.error('ERROR:', err);
-		});
-
-
-
-})
