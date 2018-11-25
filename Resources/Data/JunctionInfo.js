58f0e40fcd6e5219cb992155db392ecd4c2fda1b
Igor Soroka <stupbird@gmail.com>
2018-11-25 02:59:13 +0200

    Added mappings, new ones

--- Resources/Data/JunctionInfo.js
+++ Resources/Data/JunctionInfo.js
@@ -2,11 +2,12 @@ module.exports = {
     "event":"Junction",
     "year":"2018",
     "location":
-        "city : Helsinki, \n venue : Aalto University, Dipoli, \n adress : Otakaari 24, Otaniemi 02150 Espoo https://bit.ly/2zm8UjW \n metro : Aalto University \n directions : \n link https://live.hackjunction.com/location/ \n by plane Take train or P towards Helsinki. At the last stop, the \n city Central station, switch to a metro, and head to Matinkyla direction, leave the train on Aalto university station, \n by metro Take a ride to 'Alto university', there you will find Dipoli with ease as it is Lighted with purple Colors it is the easiest way https://bit.ly/2zm8UjW \n easiest way : The easiest way to get there is by metro,Take a ride to 'Alto university', there you will find Dipoli with ease as it is Lighted with purple Colors https://bit.ly/2zm8UjW \n",
+        "city : https://bit.ly/2zm8UjW",
     "parking": "The venue does not have designated parking, but there are some nearby, you'd better come by public transport",
     "eventtime" : "month : November \n date : November 23-25 \n start : November 23 4:00pm \n end : November 25 6:00pm",
     "food": "There will be lunch, dinner and breakfast served. Yummy! ",
     "schedule":"You can view the complete schedule here: https://live.hackjunction.com/schedule/",
     "rules":"1)No code written before the event. 2)Allowed team size is 1 – 5. 3)No remote participation. 4)You must participate in at least one challenge. 5)You must submit a link to your source.",
     "track":"There are different tracks: Blockchain, Intelligent Infrastructure, Future Cities, Digital Retail, Smart Cloud, Mobility, Data Economics, Game Jam,Internet of Things, HealthTech, AI and Big Data, more information on https://live.hackjunction.com/tracks/",
+    "greetings" : "Hello"
 };
